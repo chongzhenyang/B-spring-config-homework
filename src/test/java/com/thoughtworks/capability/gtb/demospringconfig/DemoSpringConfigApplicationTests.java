bdc597a90e3c194e4dba7a5d83fdd75038ccb1de
chongzhenyang <449559485ycz@gmail.com>
2021-03-09 16:01:47 +0800

    [chongzhen yang] test: override property file in test

--- src/test/java/com/thoughtworks/capability/gtb/demospringconfig/DemoSpringConfigApplicationTests.java
+++ src/test/java/com/thoughtworks/capability/gtb/demospringconfig/DemoSpringConfigApplicationTests.java
@@ -1,13 +1,27 @@
 package com.thoughtworks.capability.gtb.demospringconfig;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.http.ResponseEntity;
 
-@SpringBootTest
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SpringBootTest(classes = DemoSpringConfigApplication.class, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class DemoSpringConfigApplicationTests {
+
+	@LocalServerPort
+	private int port;
+
+	@Autowired
+	private TestRestTemplate restTemplate;
 
 	@Test
 	void contextLoads() {
+		ResponseEntity<String> responseEntity = restTemplate
+				.getForEntity("http://localhost:" + port + "/level", String.class);
+		assertEquals("advanced", responseEntity.getBody());
 	}
-
 }
